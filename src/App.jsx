07833bdb4188c495f1582c6d67a9808f987c1cdd
syed mohammed sazid ullah <mdsazid9089@gmail.com>
2025-05-21 03:30:56 +0600

    upadetd graphics

--- src/App.jsx
+++ src/App.jsx
@@ -1,85 +1,103 @@
-import React from 'react'
-import {useGSAP } from "@gsap/react";
+import React, { useState } from 'react';
+import { useGSAP } from "@gsap/react";
 import { gsap } from "gsap";
-import { useState } from 'react';
+import 'remixicon/fonts/remixicon.css'
 export const App = () => {
-   let [showContent, setshowContent] = useState(false)
-  useGSAP(()=>{
-    const tl = gsap.timeline()
+  const [showContent, setShowContent] = useState(false);
+
+  useGSAP(() => {
+    const tl = gsap.timeline();
     tl.to('.vi-mask-group', {
       rotate: 10,
       duration: 2,
       ease: 'power4.easeInOut',
       transformOrigin: '50% 50%',
     })
-    .to('.vi-mask-group', {
-      scale: 10,
-      duration: 2,
-      delay: -1.8,
-      ease: 'expo.easeInOut',
-      transformOrigin: '50% 50%', 
-      opacity: 0,
-      onUpdate: function () {
-        if(this.progress()>= .9){
-          document.querySelector('.svg').remove();
-          setshowContent(true)
-          this.kill()
+      .to('.vi-mask-group', {
+        scale: 10,
+        duration: 2,
+        delay: -1.8,
+        ease: 'expo.easeInOut',
+        transformOrigin: '50% 50%',
+        opacity: 0,
+        onUpdate: function () {
+          if (this.progress() >= 0.9) {
+            const svgEl = document.querySelector('.svg');
+            if (svgEl) svgEl.remove();
+            setShowContent(true);
+            this.kill();
+          }
         }
-        }
-    })
-})
+      });
+  }, []);
+
   return (
     <>
-     
-      <div className="svg flex items-center justify-center fixed top-0 left-0 z-[100] w-full h-screen overflow-hidden bg-[#000]"> 
-       <svg className="0 0 800 600" preserveAspectRatio='xMidYMid slice'>
-        <defs>
-          <mask id="viMask">
-            <react width="100%" height="100%" fill="black"/>
+      <div className="svg flex items-center justify-center fixed top-0 left-0 z-[100] w-full h-screen overflow-hidden bg-[#000]">
+        <svg viewBox="0 0 800 600" className="w-full h-full" preserveAspectRatio="xMidYMid slice">
+          <defs>
+            <mask id="viMask">
+              <rect width="100%" height="100%" fill="black" />
               <g className="vi-mask-group">
                 <text
                   x="50%"
                   y="50%"
                   fontSize="250"
                   textAnchor="middle"
-                  fill='white'
+                  fill="white"
                   dominantBaseline="middle"
-                  fontFamily="Arial black"
-                  >
-                    VI
-                    </text>
+                  fontFamily="Arial Black"
+                >
+                  VI
+                </text>
               </g>
-            
-          </mask>
-        </defs>
-        <image
-        href="./bg.png"
-        width="100%"
-        height="100%"
-        preserveAspectRatio="xMidYMid slice"
-        mask="url(#viMask)"
-        />
-       </svg>
-    </div>
-    {showContent && (
-      <div className='main w-full '>
-        <div className='landing w-full h-screen bg-black'>
-          <div className='navbar absolute top-0 left-0 w-full z-[10] py-10 px-10'></div>
-        <div className="imagesdiv relative w-full h-full object-cover">
-          <img className="absolute top-0 left-0 w-full h-full object-cover" src=".\sky.png" alt=""/>
-          <img className="absolute top-0 left-0 w-full h-full object-cover" src=".\bg.png" alt=""/>
-           <div className="text text-white flex flex-col gap-3 absolute top-20 left-1/2 -translate-x-1/2 scale-[1.4] rotate-[-10deg]">
-                <h1 className="text-[12rem] leading-none -ml-40">grand</h1>
-                <h1 className="text-[12rem] leading-none ml-20">theft</h1>
-                <h1 className="text-[12rem] leading-none -ml-40">auto</h1>
+            </mask>
+          </defs>
+          <image
+            href="./bg.png"
+            width="100%"
+            height="100%"
+            preserveAspectRatio="xMidYMid slice"
+            mask="url(#viMask)"
+          />
+        </svg>
+      </div>
+      {showContent && (
+        <div className="main w-full">
+          <div className="landing w-full h-screen bg-black">
+            <div className="navbar absolute top-0 left-0 w-full z-[10] py-10 px-10">
+              <div className="logo flex gap-7">
+                <div className="lines flex flex-col gap-[5px]">
+                  <div className="line w-10 h-2 bg-white"></div>
+                  <div className="line w-8 h-2 bg-white"></div>
+                  <div className="line w-5 h-2 bg-white"></div>
+                </div>
+                <div className="text-4xl -mt-[8px] leading-none text-white">Rockstar</div>
               </div>
-          <img className="absolute -bottom-[150%] left-1/2 -traslate-x-1/2 scale-[3]" src=".\girlbg.png" alt=""/>
-
+            </div>
+            <div className="imagesdiv relative overflow-hidden w-full h-full object-cover">
+              <img className="absolute top-0 left-0 w-full h-full object-cover" src="./sky.png" alt="" />
+              <img className="absolute top-0 left-0 w-full h-full object-cover" src="./bg.png" alt="" />
+              <img
+                className="absolute -bottom-35 left-1/2 -translate-x-1/2 w-[210px] md:w-[150px] lg:w-[450px] transition-all duration-500 ease-in-out"
+                src="./girlbg.png"
+                alt="Girl"
+              />
+            </div>
+            <div className='btmbar  text-white bottom-0 left-0 absolute w-full py-15 px-10 bg-gradient-to-t from-black to-transparent'>
+              <div className='flex gap-4 items-center' >
+                <i class="text-3xl ri-arrow-down-line"></i>
+                <h3 className='font-[helvetica] text-xl font-bold'>
+                  Scroll down
+                </h3>
+              </div>
+              <img className='absolute h-[55px] top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2' src='./ps5.png' alt='ps5' />
+            </div>
+          </div>
         </div>
-      </div>
-      </div>
-    )}
+      )}
     </>
-  )
-}
-export default App
+  );
+};
+
+export default App;
